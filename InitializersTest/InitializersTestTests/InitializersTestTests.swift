d05eaa0fb96ba18af12775a9adb262277f336425
gurjot1829 <156744445+gurjot1829@users.noreply.github.com>
2025-11-03 10:30:35 +0530

    Add files via upload

---
+++ InitializersTest/InitializersTestTests/InitializersTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  InitializersTestTests.swift
+//  InitializersTestTests
+//
+//  Created by Gurjot Singh on 03/11/25.
+//
+
+import Testing
+@testable import InitializersTest
+
+struct InitializersTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
